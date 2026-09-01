c6457bd68c7963e71e01e68327beb182aaaee084
MaheshP-Credera <mahesh.p@credera.com>
2026-09-01 18:02:36 +0530

    Form update

--- tests/FormTest.spec.js
+++ tests/FormTest.spec.js
@@ -10,7 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 dotenv.config({ path: path.resolve(__dirname, '../Env/.env'), override: true });
-const formData = readJsonData('../test-data/formData.json');
+const formData = readJsonData(__dirname, '../test-data/formData.json');
 
 test('Submit valid registration form', async ({ page }) => {
     const formPage = new FormPage(page);
